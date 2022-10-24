fb5e31733b6cf693dd3ba25f318ad434a6c4d8c6
annarokalo <rokalo.a.s@gmail.com>
2022-10-24 14:44:49 +0300

    SP: Add Flex component, update svg for clarityEye icon, add prettier on pre-commit

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box, GridContainer, Grid, GridItem, Logo, Title, Spacer } from "@/shared/components"
+import { Box, GridContainer, Grid, GridItem, Logo, Title, Spacer, Flex } from "@/shared/components"
 import { CommonLayout } from "@/shared/layouts/common"
 
 import { LoginFormContainer } from "./LoginFormContainer"
@@ -8,32 +8,39 @@ export const Login = () => {
   return (
     <CommonLayout>
       <GridContainer fullBleed>
-        <Grid columns={{ "@initial": "1fr", "@sm": "1fr 1fr" }} css={{ "@sm": { height: '100vh' }, height: '100%'}}>
-          <GridItem css={{
-            backgroundColor: "$neutrals-1",
-            padding: '$32 $24',
-            display: "flex",
-            flexFlow: 'column nowrap',
-            justifyContent: 'center'
-          }} >
-            <Box css={{flex: '0 0 auto',}}>
+        <Grid
+          columns={{ "@initial": "1fr", "@sm": "1fr 1fr" }}
+          css={{ "@sm": { height: "100vh" }, height: "100%" }}
+        >
+          <GridItem
+            css={{
+              backgroundColor: "$neutrals-1",
+              padding: "$32 $24",
+              display: "flex",
+              flexFlow: "column nowrap",
+              justifyContent: "center",
+            }}
+          >
+            <Box css={{ flex: "0 0 auto" }}>
               <Logo />
             </Box>
             <Spacer size={32} />
-            <Box
+            <Flex
+              justify="center"
+              direction="column"
               css={{
-                display: "flex",
-                justifyContent: "center",
-                flexDirection: "column",
-                flex: '1 1 100%',
+                flex: "1 1 100%",
               }}
-              >
-                <Title css={{marginBottom: '$24'}} as='h2'>Welcome!</Title>
-                <LoginFormContainer />
-              </Box>
-
+            >
+              <Title css={{ marginBottom: "$24" }} as="h2">
+                Welcome!
+              </Title>
+              <LoginFormContainer />
+            </Flex>
           </GridItem>
-          <GridItem css={{ backgroundColor: "$brand-blue-lightest", minHeight: '480px'}}></GridItem>
+          <GridItem
+            css={{ backgroundColor: "$brand-blue-lightest", minHeight: "480px" }}
+          ></GridItem>
         </Grid>
       </GridContainer>
     </CommonLayout>
