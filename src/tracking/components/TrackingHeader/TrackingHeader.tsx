c329c544d8183553d1334a87d7002f1300da76e8
VladKarchenia <karchenia@mail.ru>
2022-11-14 17:28:45 +0300

    Feature: Add edit shipment button

--- src/tracking/components/TrackingHeader/TrackingHeader.tsx
+++ src/tracking/components/TrackingHeader/TrackingHeader.tsx
@@ -1,5 +1,6 @@
-import { Copy, Flex, GridContainer, Stack } from "@/shared/components"
+import { ButtonIcon, Copy, Flex, GridContainer, Stack } from "@/shared/components"
 import { StatusLabel } from "@/shared/components/app"
+import { IconPencil } from "@/shared/icons"
 import { ShipmentStatus } from "@/shared/types"
 
 interface ITrackingHeader {
@@ -11,11 +12,18 @@ export const TrackingHeader = ({ shipmentID, shipmentDate }: ITrackingHeader) =>
   return (
     <GridContainer>
       <Stack space={8}>
-        <Flex align="center">
-          <Copy scale={8} color="system-black" bold css={{ paddingRight: "$12" }}>
-            Ship #{shipmentID}
-          </Copy>
-          <StatusLabel status={ShipmentStatus.Confirmed} />
+        <Flex align="center" justify="between">
+          <Flex align="center">
+            <Copy scale={8} color="system-black" bold css={{ paddingRight: "$12" }}>
+              Ship #{shipmentID}
+            </Copy>
+            <StatusLabel status={ShipmentStatus.Confirmed} />
+          </Flex>
+          <ButtonIcon
+            ariaLabel="Edit shipment"
+            icon={<IconPencil />}
+            onClick={() => console.log("Clicked edit button")}
+          />
         </Flex>
         <Copy scale={10}>{shipmentDate}</Copy>
       </Stack>
