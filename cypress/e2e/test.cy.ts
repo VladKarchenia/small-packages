52d9046cbd2b2ecc8b09c313b220eb9b1aef0c4a
VladKarchenia <karchenia@mail.ru>
2023-05-17 14:36:07 +0300

    Add test cy file

---
+++ cypress/e2e/test.cy.ts
@@ -0,0 +1,12 @@
+describe("Dashboard Page", () => {
+  beforeEach(() => {
+    cy.visit("/")
+  })
+
+  it("redirects to create a shipment page after button click", () => {
+    cy.get('[data-testid="create-button"]').click()
+    cy.get('[data-testid="create-shipment"]').click()
+
+    cy.url().should("eq", "http://localhost:3000/create/shipment")
+  })
+})
